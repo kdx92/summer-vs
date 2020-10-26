a87cce64da85836bd93a92cde8f6552635b386a0
kdx92 <18761925416@163.com>
2020-10-26 16:25:33 +0800

    work

--- star/main.js
+++ star/main.js
@@ -1,40 +1,69 @@
-// onload = function(){
-//   //获取所有的li
-//   var list = document.getElementsByTagName("li");
-//   //1.经过所有的li标签时，点亮自己以及前面的兄弟
-//   //2.不点击后离开所有的li标签时，所有li都熄灭
-//   //3.点击后离开时，点亮自己以及前面的兄弟
-//   for(var i =0;i<list.length;i++){
-//     list[i].index = i;//循环的时候保存li标签对应的索引值
-//     list[i].onmouseover = function(){
-//       for(var j=0;j<=this.index;j++){
-//         list[j].style.backgroundPosition = "0px 0px";
-//       }
-//     };
-//     list[i].onmouseout = function(){
-//       for(var k=0;k<list.length;k++){
-//         list[k].style.backgroundPosition = "0px -40px";
-//       }
-//       //判断有没有current，有的话让current和前面的兄弟点亮
-//       var current = document.getElementById("current");
-//       if(current == null){return;}
-//       var cur_index = current.index;
-//       for(var p=0;p<=cur_index;p++){
-//         list[p].style.backgroundPosition ="0px 0px";
-//       }
-//     };
-//     list[i].onclick = function(){
-//       //给所点击的li标签,添加一个标识
-//       //在离开时 可以根据有没有这个标识来判断 前一次操作是点击
-//       for(var m=0;m<list.length;m++){
-//         list[m].id = "";
-//       }
-//       this.id = "current";
-//     };
-//   }
-// };
+function overhandle(obj){
+  var fnode = obj.parentNode;
+  var imglist = fnode.getElementsByClassName('score');
+  for(var i=0;i<imglist.length;i++){
+      imglist[i].style['background-image'] = 'img/star.png';
+      imglist[i].style['background-position'] = '0px 40px';
 
+      imglist[i].style.width = '40px';
+      imglist[i].style.height = '40px';
+      imglist[i].style['background-size'] = '40px 80px';
 
+  }
+  var node = obj;
+  var index = 0;
+  while(node = node.previousSibling){
+      if(node.nodeType == 1){
+          index ++;
+      }
+  }
+  node = obj;
+  for(;index>=2;){
+      if(node.nodeType == 1){
+          node.style['background-image'] = 'img/star.png';
+          node.style['background-position'] = '0px 0px';
+          node.style.width = '40px';
+          node.style.height = '40px';
+          node.style['background-size'] = '40px 80px';
+          index--;
+      }
+      node = node.previousSibling;
+  }
+}
+function outhandle(obj){
+  var fnode = obj.parentNode;
+  var list = fnode.getElementsByClassName('starvote');
+  var imglist = fnode.getElementsByClassName('score');
+  for(var i=0;i<imglist.length;i++){
+      imglist[i].style['background-image'] = 'img/star.png';
+      imglist[i].style['background-position'] = '0px 40px';
+      imglist[i].style.width = '40px';
+      imglist[i].style.height = '40px';
+      imglist[i].style['background-size'] = '40px 80px';
+  }
+  for(var i=0;i<list[0].value;i++){
+      imglist[i].style['background-image'] = 'img/star.png';
+      imglist[i].style['background-position'] = '0px 0px';
+      imglist[i].style.width = '40px';
+      imglist[i].style.height = '40px';
+      imglist[i].style['background-size'] = '40px 80px';
+
+  }
+}
+function clickhandle(obj){
+  var fnode = obj.parentNode;
+  var list = fnode.getElementsByClassName('starvote');
+  var node = obj;
+  var index= 0;
+  while(node =  node.previousSibling){
+      if(node.nodeType == 1){
+          index++;
+      }
+  }
+  list[0].value = index-1;
+}
+
+// 另一个
 var word = document.getElementById("input");
 var submit = document.getElementById("button");
 
