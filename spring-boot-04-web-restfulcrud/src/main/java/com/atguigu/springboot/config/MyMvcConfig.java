08c26bcfb0e1e9fc75770d90bd5cf68b0ac5bb64
kdx92 <18761925416@163.com>
2020-11-21 19:23:24 +0800

    learn

--- spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/config/MyMvcConfig.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/config/MyMvcConfig.java
@@ -1,8 +1,13 @@
 package com.atguigu.springboot.config;
 
+import com.atguigu.springboot.component.LoginHandlerInterceptor;
+import com.atguigu.springboot.component.MyLocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.*;
+
+import java.util.Locale;
 
 //使用WebMvcConfigurerAdapter可以扩展SpringMVC的功能
 //@EnableWebMvc
@@ -20,18 +25,31 @@ public class MyMvcConfig implements WebMvcConfigurer {
 //    }
 
 //    所有的WebMvcConfigurerAdapter组件都会一起起作用
-    @Bean
 // 将组件注册在容器
+    @Bean
     public WebMvcConfigurer webMvcConfigurerAdapter(){
         WebMvcConfigurer configuer = new WebMvcConfigurer(){
             @Override
             public void addViewControllers(ViewControllerRegistry registry) {
 //                super.addViewControllers(registry);
                 registry.addViewController("/").setViewName("login");
-                registry.addViewController("/login.html").setViewName("login");
+                registry.addViewController("/index.html").setViewName("login");
+                registry.addViewController("/main.html").setViewName("dashboard");
+            }
+
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+//                super.addInterceptors(registry);
+                registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
+                        .excludePathPatterns("/index.html","/","/user/login","/webjars/**","/asserts/**");
             }
         };
         return configuer;
 
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
     }
 }
---
+++ spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/controller/EmployeeController.java
@@ -0,0 +1,45 @@
+package com.atguigu.springboot.controller;
+
+import com.atguigu.springboot.dao.DepartmentDao;
+import com.atguigu.springboot.dao.EmployeeDao;
+import com.atguigu.springboot.entities.Department;
+import com.atguigu.springboot.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    EmployeeDao employeeDao;
+
+    @Autowired
+    DepartmentDao departmentDao;
+
+//    查询所有员工返回列表页面
+    @GetMapping("/emps")
+    public String list(Model model){
+        Collection<Employee> employees = employeeDao.getAll();
+
+//        放在请求域中
+        model.addAttribute("emps",employees);
+//        thymeleaf默认就会拼串
+//        classpath:/templates/xxx.html
+        return "emp/list";
+    }
+
+//    来到员工添加页面
+    @GetMapping("/emp")
+    public String toAddPage(Model model){
+
+//        来到添加页面
+//        查出所有的部门，在页面显示
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        return "emp/add";
+    }
+}
