7c33aab5cdd6004f454c2994969f857cb7e3fdb2
kdx92 <18761925416@163.com>
2020-11-22 18:48:25 +0800

    th

--- spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/controller/EmployeeController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/controller/EmployeeController.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Collection;
 
@@ -42,4 +45,44 @@ public class EmployeeController {
         model.addAttribute("depts",departments);
         return "emp/add";
     }
+//    员工添加
+//    SpringMVC自动将请求参数和入参对象的属性进行一一绑定：要求请求参数的名字和javaBean入参的对象里面的属性名是一样的
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+//        来到员工列表页面
+//        redirect：表示重定向到一个地址
+//        forward：表示转发到一个地址
+//        /表示当前项目路径
+
+//        保存员工
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+//    来到修改页面，查出当前员工，在页面回显
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable("id") Integer id,Model model){
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp",employee);
+
+//        页面要显示所有的部门列表
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+//        回到修改页面（add页面是一个修改添加二合一的页面）
+        return "emp/add";
+
+
+
+
+    }
+
+//    员工修改：需要提交员工id
+
+    @PutMapping("/emp")
+    public String updateEmployee(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/emps";
+
+    }
+
 }
