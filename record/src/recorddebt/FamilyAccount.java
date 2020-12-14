ef9060277dbcef6f21bb112d36bc8d760ffb42de
kdx92 <18761925416@163.com>
2020-12-14 18:22:48 +0800

    submit

--- record/src/recorddebt/FamilyAccount.java
+++ record/src/recorddebt/FamilyAccount.java
@@ -7,21 +7,26 @@ public class FamilyAccount {
 
 		boolean isFlag = true;
 
+		String details = "收支\\t账户金额\\t收支金额\\t说明\n";
+
 		while (isFlag) {
 
 			System.out.println("--------收支记录--------\n");
-			System.out.println("                       1.收支明细");
-			System.out.println("                       2.登记收入");
-			System.out.println("                       3.登记支出");
-			System.out.println("                       4.退出\n");
-			System.out.print("                         请选择1-4输入\n");
+			System.out.println("        1.收支明细");
+			System.out.println("        2.登记收入");
+			System.out.println("        3.登记支出");
+			System.out.println("        4.退出\n");
+			System.out.print("          请选择1-4输入\n");
 
 //			获取用户的输入
 			char selection = Utility.readMenuSelection();
 
 			switch (selection) {
 			case '1':
-				System.out.println("1.收支明细");
+//				System.out.println("1.收支明细");
+				System.out.println("--------当前收支明细记录--------");
+				System.out.println(details);
+				System.out.println("----------------------");
 				break;
 
 			case '2':
@@ -33,8 +38,14 @@ public class FamilyAccount {
 				break;
 
 			case '4':
-				System.out.println("4.退出");
-				break;
+//				System.out.println("4.退出");
+				System.out.print("确认是否退出（Y/N）：");
+				char isExit = Utility.readConfirmSelection();
+				if (isExit == 'Y') {
+					isFlag = false;
+
+				}
+//				break;
 
 //			default:
 //				break;
